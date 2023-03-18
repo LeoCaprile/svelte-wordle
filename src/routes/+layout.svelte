10822c99c2560344b4491394fe52ced451a380a7
Matías Fuentes <mfuentes@jumpitt.com>
2023-03-17 23:10:37 -0300

    :lipstick: Added title and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,28 @@
 	import Page from './+page.svelte';
 </script>
 
-<div>
+<div class="app">
+	<header>Sverdle</header>
 	<Page />
+	<footer>Build by Matias Fuentes (LeoCaprile)</footer>
 </div>
 
 <style lang="scss">
-	div {
+	@import url('https://fonts.googleapis.com/css2?family=Rubik+Iso&display=swap');
+	.app {
 		display: flex;
+		flex-direction: column;
+		align-items: center;
 		justify-content: center;
+
+		footer {
+			margin-top: 40px;
+			margin-bottom: 40px;
+		}
+	}
+
+	header {
+		font-size: 100px;
+		font-family: 'Rubik Iso', cursive;
 	}
 </style>
